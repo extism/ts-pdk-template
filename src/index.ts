420dbcc20a7f24c4855da8a4f6b077a5f88c0450
Steve Manuel <steve@dylibso.com>
2024-04-02 16:48:35 -0600

    feat: initial template

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export function greet() {
+    let extra = new TextEncoder().encode("aaa")
+    let decoded = new TextDecoder().decode(extra)
+    Host.outputString(`Hello, ${Host.inputString()} ${decoded}`)
+}
